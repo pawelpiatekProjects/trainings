0acdac93abe69d2f489626b176e78c98df3420d7
Pawel Piatek <pawelpiatek98@gmail.com>
2020-08-08 13:56:37 +0200

    fixed hero img on home page

--- src/components/home/contentRight.js
+++ src/components/home/contentRight.js
@@ -1,6 +1,6 @@
 import React, {useRef, useEffect} from 'react';
 import styled from 'styled-components';
-import {ReactComponent as HeroImg} from '../../assets/images/svg/grafika1.1 1.svg';
+import {ReactComponent as HeroImg} from '../../assets/images/svg/home-hero.svg';
 import gsap from 'gsap';
 
 const ContentRightWrapper = styled.div`
@@ -13,10 +13,10 @@ const HeroImgWrapper = styled.div`
   position: relative;
   
   svg{
-    height: 85vh;
+    height: 75vh;
     width: 100%;
     position: absolute;
-    bottom: 1rem;
+    bottom:5rem;
     left: 50%;
     transform: translateX(-50%);
   }
@@ -31,28 +31,19 @@ const ContentRight = () => {
 
         const body = elements.getElementById('body');
         const lines = elements.getElementById('lines');
-        const line1 = elements.getElementById('line1');
-        const line2 = elements.getElementById('line2');
-        const line3 = elements.getElementById('line3');
-        const line4 = elements.getElementById('line4');
 
 
 
-        gsap.set([body, line1, line2, line3, line4], {autoAlpha: 0});
+
+        gsap.set([body, lines], {autoAlpha: 0});
         gsap.set(lines, {transformOrigin: '50% 50%'});
-        gsap.set(line1, {transformOrigin: '50% 50%'});
-        gsap.set(line2, {transformOrigin: '50% 50%'});
-        gsap.set(line3, {transformOrigin: '50% 50%'});
-        gsap.set(line4, {transformOrigin: '50% 50%'});
+
 
         const tl = gsap.timeline({defaults: {ease: 'power3.inOut'}});
 
         tl.fromTo(body, {x: '-=300'}, {duration: 1, x: '+=300', autoAlpha: 1})
             .fromTo(lines, {scaleY: .85}, {ease:'power3.inOut', duration: .4, autoAlpha: 1, scaleY: 1})
-            // .fromTo(line1, {scaleY: 0}, {ease:'power3.inOut', duration: .2, autoAlpha: 1, scaleY: 1})
-            // .fromTo(line2, {scaleY: 0}, {ease:'power3.inOut', duration: .2, autoAlpha: 1, scaleY: 1})
-            // .fromTo(line3, {scaleY: 0}, {ease:'power3.inOut', duration: .2, autoAlpha: 1, scaleY: 1})
-            // .fromTo(line4, {scaleY: 0}, {ease:'power3.inOut', duration: .2, autoAlpha: 1, scaleY: 1})
+
     })
 
     return(
