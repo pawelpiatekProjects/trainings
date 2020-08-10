690c9cf3f0770aa221e8fa95f9511175f7166a3e
Pawel Piatek <pawelpiatek98@gmail.com>
2020-08-10 22:13:32 +0200

    finish 10.08

--- src/components/dashboard/lastTraining.js
+++ src/components/dashboard/lastTraining.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import * as variables from '../../assets/variables';
+import {Header} from "../UIComponents/YellowBorderHeader";
 
 const LastTrainingWrapper = styled.div`
   height: 100%;
   background: ${variables.light};
+  padding: 2rem;
 `;
 
-
+const HeaderWrapper = styled.div`
+  width: 60%;
+`;
 
 const LastTraining = () => {
     return (
         <LastTrainingWrapper>
-            <p>chart right</p>
+            <HeaderWrapper>
+                <Header>Last Training</Header>
+            </HeaderWrapper>
         </LastTrainingWrapper>
     )
 };
