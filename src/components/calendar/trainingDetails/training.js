28f492a2bb666d1b6bf783d4683c15b8a7397daf
Pawel Piatek <pawelpiatek98@gmail.com>
2020-09-03 18:22:46 +0200

    added loader

--- src/components/calendar/trainingDetails/training.js
+++ src/components/calendar/trainingDetails/training.js
@@ -2,18 +2,59 @@ import React from 'react';
 import styled from 'styled-components';
 import * as variables from '../../../assets/variables';
 import PropTypes from 'prop-types';
+import Loader from 'react-loader-spinner';
 
 const TrainingWrapper = styled.div`
+  background: ${variables.light};
+  padding: 3rem;
+`;
+
+const TrainingHeader = styled.h1`
 
 `;
 
+const Exercise = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+  margin: 0 auto;
+`;
+
+const ExerciseItem = styled.li`
+   //display: inline-block;
+   //margin-right: .5rem;
+   font-size: 1.6rem;
+`;
+
+
+//todo: add loader before displaying selected training
 const Training = ({activeTraining}) => {
     console.log(activeTraining)
+
+    let renderedElement =
+        <Loader
+            type="BallTriangle"
+            color={variables.yellowPrimary}
+            height={100}
+            width={100}
+            // timeout={3000} //3 secs
+
+        />
+
+    //todo: move loader upper
     return (
         <TrainingWrapper>
-            <h1>{activeTraining.date}</h1>
+            {renderedElement}
+            {/*<TrainingHeader>{activeTraining.date}</TrainingHeader>*/}
             {/*{activeTraining.exercises.map(exercise => (*/}
-            {/*    <p>{exercise}</p>*/}
+
+            {/*    <Exercise>*/}
+            {/*        <ExerciseItem>{exercise.name}</ExerciseItem>*/}
+            {/*        <ExerciseItem>{exercise.series}</ExerciseItem>*/}
+            {/*        <ExerciseItem>{exercise.reps}</ExerciseItem>*/}
+            {/*    </Exercise>*/}
+
             {/*))}*/}
         </TrainingWrapper>
     )
